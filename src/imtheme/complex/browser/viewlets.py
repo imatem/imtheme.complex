3b10998e2500ff32301b86deeb7791a45ab42871
adriana-rv <adriana.rv@gmail.com>
2019-06-06 12:43:51 -0500

    Extend searchbox class

--- src/imtheme/complex/browser/viewlets.py
+++ src/imtheme/complex/browser/viewlets.py
@@ -1,4 +1,6 @@
 from plone.app.layout.viewlets import ViewletBase
+from Products.CMFCore.utils import getToolByName
+from plone.app.layout.viewlets.common import SearchBoxViewlet
 
 
 class ActivitiesViewlet(ViewletBase):
@@ -7,3 +9,32 @@ class ActivitiesViewlet(ViewletBase):
 
 class LogosViewlet(ViewletBase):
     pass
+
+
+class IMSearchBoxViewlet(SearchBoxViewlet):
+
+    def is_anonymous(self):
+        portal_membership = getToolByName(self.context, 'portal_membership', None)
+        return portal_membership.isAnonymousUser()
+
+    def user_name_home(self):
+
+        portal_membership = getToolByName(self.context, 'portal_membership')
+        # authenticated_user = portal_membership.getAuthenticatedMember().getProperty('location')
+        userid = portal_membership.getAuthenticatedMember().id
+        memberinfo = portal_membership.getMemberInfo(userid)
+
+        if memberinfo:
+            fullname = memberinfo.get('fullname', '')
+        else:
+            fullname = None
+        if not fullname:
+            fullname = memberinfo.get('username', '')
+
+        homelink = "%s/useractions" % self.navigation_root_url
+
+        return {'name': fullname, 'homelink': homelink}
+
+    def gologin(self):
+        return "%s/login" % self.navigation_root_url
+
