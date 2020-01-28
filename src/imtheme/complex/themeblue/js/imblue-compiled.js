363901594499e42f1eef580853ae77ad66d433d7
adriana-rv <adriana.rv@gmail.com>
2020-01-28 12:31:25 -0600

    Include javascript for slider header

---
+++ src/imtheme/complex/themeblue/js/imblue-compiled.js
@@ -0,0 +1,49 @@
+/*For gigant  activities slider*/
+
+var slideIndex = 0;
+showSlides();
+
+function plusSlides(n) {
+  showSlides2(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides2(slideIndex = n);
+}
+
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {
+    slideIndex = 1;
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+  setTimeout(showSlides, 6000); // Change image every x seconds
+}
+
+
+function showSlides2(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
+
